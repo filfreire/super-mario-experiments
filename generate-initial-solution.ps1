6150450a07f1574371de1b74bb0d4293510b0f42
Filipe Freire <livrofubia@gmail.com>
2023-12-20 10:53:49 +0000

    Update generate-initial-solution.ps1

--- generate-initial-solution.ps1
+++ generate-initial-solution.ps1
@@ -1,4 +1,6 @@
 # Created by Filipe Freire, 2023
 # This script generates an feasible initial solution for the SMB game
 
-fceux.exe -loadstate .\savestates\smb.fc0 -lua C:\Users\filipe\Desktop\metaheuristics\super-mario-experiments\scripts\mario-initial-generation.lua -bginput 1 .\roms\smb.nes
+$scriptPath = $PSScriptRoot
+$fullLuaScriptPath = Join-Path -Path $scriptPath -ChildPath "\scripts\mario-initial-generation.lua"
+fceux.exe -loadstate .\savestates\smb.fc0 -lua $fullLuaScriptPath -bginput 1 .\roms\smb.nes
